2de96d39252f39a5916fdc4e58a77b6e6b3e60e1
zhanglibin.buaa <zhanglibin.buaa@bytedance.com>
2022-03-04 18:47:02 +0800

    add Herbarium dataset

--- util/Herbarium.py
+++ util/Herbarium.py
@@ -1,27 +1,70 @@
 import cv2
-from torchvision.datasets import ImageFolder
+from torchvision.datasets import VisionDataset
 # torchvision.datasets.ImageFolder
+import json
+from PIL import Image
 
-class Herbarium2022(ImageFolder):
+
+class Herbarium2022(VisionDataset):
     '''
         15501 classes 
     '''
-    def __init__(self, root_dir, df, transforms=None):
-        self.root_dir = root_dir
-        self.df = df
-        self.transforms = transforms
+    def __init__(self, root, anno_file, transform=None, target_transform = None):
+        super(VisionDataset, self).__init__(root, transform=transform,target_transform=target_transform)
+        self.root_dir = root
+        self.transform = transform
+        # if target_transform is not None:
+        self.target_transform = target_transform
+        self.anno_file = anno_file
+
+        # open anno file 
+        print('loading dataset annotation file ...')
+        anno_f = open(self.anno_file, 'r')
+
+        anno = json.load(anno_f)
+        annotations = anno['annotations']
         
+        self.samples = []
+        for item in annotations:
+            category_id = item['category_id']
+            image_id = item['image_id']
+            # (image_id, cat_id)
+            self.samples.append((image_id, category_id))
+        
+        #prepare cat_id to cat_name
+        categories = anno['categories']
+        self.catid2catname = dict()
+        for cat in categories:
+            category_id = cat['category_id']
+            scientificName = cat['scientificName']
+            self.catid2catname[category_id] = scientificName
+        
+        anno_f.close()
+        print('load datasets done !!!')
+        print('dataset len: {}, class nums : {}'.format(self.__len__(), len(self.catid2catname)))
+    
+                
     def __len__(self):
-        return len(self.df)
+        return len(self.samples)
     
     def __getitem__(self, idx):
-        filename = self.df.iloc[idx, 0]
-        image_path = os.path.join(TRAIN_DIR, filename)
-        img = cv2.imread(image_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        label = self.df.iloc[idx, 5]
+        image_id, label = self.samples[idx]
+        # image_id : 00147__049
+        sub_folder1 = image_id[:3]
+        sub_folder2 = image_id[3:5]
+
+        image_path = self.root_dir + '/' + sub_folder1 + '/' + sub_folder2 + '/'+ image_id + '.jpg'
+        img = self.pil_loader(image_path)
         
-        if self.transforms is not None:
-            img = self.transforms(image=img)["image"]
+        if self.transform is not None:
+            img = self.transform(image=img)["image"]
+        if self.target_transform is not None :
+            label = self.target_transform(label)
             
         return img, label
+
+    def pil_loader(self, path: str) -> Image.Image:
+        # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
